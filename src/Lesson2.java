f418b625b00a63100e5dbfef768923cccf93dd56
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-10-20 19:32:44 +0300

    Done4

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -9,7 +9,7 @@ public class Lesson2 {
         printColor();
         compareNumbers();
     }
-
+    // Задание 1
     public static void printThreeWords() {
 
         String firstWord = "Orange";
@@ -18,7 +18,7 @@ public class Lesson2 {
 
         System.out.print(firstWord + "\n" + secondWord + "\n" + thirdWord + "\n");
     }
-
+    // Задание 2
     public static void checkSumSign() {
         int a = 1;
         int b = 2;
@@ -28,13 +28,13 @@ public class Lesson2 {
 
 
     }
-
+    // Задание 3
     public static void printColor() {
         int value = 0;
         String result = (value <= 0) ? "Красный" : (value > 0 & value <= 100) ? "Желтый" : "Зеленый";
         System.out.println(result);
     }
-
+    // Задание 4
     public static void compareNumbers() {
         int a = 20;
         int b = 2;
