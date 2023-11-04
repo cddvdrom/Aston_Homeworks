2b6aa09846ef77830fd328b8073a5a577c5e2528
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-05 00:49:27 +0300

    1 коммит

--- src/task1/View/WindowsDisplay.java
+++ src/task1/View/WindowsDisplay.java
@@ -1,7 +1,9 @@
 package task1.View;
 
 import task1.Result.Result;
-
+/*
+Пока не реализовано
+ */
 public class WindowsDisplay implements DisplayResult{
     @Override
     public void show(Result result) {
