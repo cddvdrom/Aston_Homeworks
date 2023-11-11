6222adc279c1102c73417c86e2fa7787fea604d4
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-12 02:29:39 +0300

    commit

--- src/task1/service/DisplayOutput.java
+++ src/task1/service/DisplayOutput.java
@@ -9,11 +9,12 @@ public class DisplayOutput {
         Collections.sort(list);
 
         System.out.println("-----------------------------------------------------");
-        System.out.print(String.format("|%-25s | %-22s |\n", "Слово", "Количество повторений"));
+        System.out.print(String.format("|%-3s|%-25s | %-22s |\n","id", "Слово", "Количество повторений"));
         System.out.println("-----------------------------------------------------");
+        int counter=1;
         for (String value : list
         ) {
-            System.out.print(String.format("|%-25s | %22s |\n", value, map.get(value.toString())));
+            System.out.print(String.format("|%-3s|%-25s | %22s |\n",counter++, value, map.get(value.toString())));
 
         }
         System.out.println("-----------------------------------------------------");
