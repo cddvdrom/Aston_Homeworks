1ce4daf12fc377c1907c31ee35183f1fc02c1cc4
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-14 13:13:34 +0300

    commit

---
+++ src/task3/CollectionUtils.java
@@ -0,0 +1,17 @@
+package task3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectionUtils {
+    public <T> List<T> sortElementsInNaturalOrder(List<T> list) {
+        return list.stream().sorted().collect(Collectors.toList());
+    }
+
+    public List <String> sortWordsCollectionInNaturalOrder (List <String> list){
+        return list.stream().map(x -> Arrays.stream(x.split("")).
+                        sorted().collect(Collectors.toList())).
+                map(x -> x.stream().collect(Collectors.joining())).collect(Collectors.toList());
+    }
+}
--- src/task3/StartApp.java
+++ src/task3/StartApp.java
@@ -1,22 +1,18 @@
 package task3;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StartApp {
     public static void main(String[] args) {
         Data data = new Data();
+        CollectionUtils utils = new CollectionUtils();
         System.out.println("Коллекция " + data.getData());
-        // Collections.sort(list);
         System.out.println("Сортируем слова коллекции в алфавитном порядке : \n"
-                + data.getData().stream().sorted().collect(Collectors.toList()));
-        System.out.println("Сортируем каждое слово коллекции в алфавитном порядке : " + "");
-        List list = data.getData().stream().map(x -> Arrays.stream(x.split("")).
-                        sorted().collect(Collectors.toList())).
-                map(x -> x.stream().collect(Collectors.joining())).collect(Collectors.toList());
+                + utils.sortElementsInNaturalOrder(data.getData()));
+        System.out.println("Сортируем каждое слово коллекции в алфавитном порядке : ");
+        List<String> list = utils.sortWordsCollectionInNaturalOrder(data.getData());
         System.out.println(list);
         System.out.println("Отсортированная коллекция с отсортированными словами");
-        System.out.println(list.stream().sorted().collect(Collectors.toList()));
+        System.out.println(utils.sortElementsInNaturalOrder(list));
     }
 }
