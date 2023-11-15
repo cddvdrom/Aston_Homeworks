fed62c6ac86847600102664c1f4d68a0faee3bff
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-15 23:04:42 +0300

    commit

--- src/task1/WordAnalyser.java
+++ src/task1/WordAnalyser.java
@@ -3,8 +3,8 @@ package task1;
 import java.util.HashMap;
 
 public class WordAnalyser {
-    public HashMap <String, Integer> analyze(String [] array) {
-        HashMap <String, Integer> map = new HashMap<>();
+    public HashMap <String , Integer> analyze(String [] array) {
+        HashMap <String , Integer> map = new HashMap<>();
         for (String line : array) {
             if (map.isEmpty() || !map.containsKey(line)) {
                 map.put(line, 1);
