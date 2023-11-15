1117d1508447e1b190eb274a8d3204a308ccacee
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-15 23:03:14 +0300

    commit

--- src/task1/StartApp.java
+++ src/task1/StartApp.java
@@ -2,11 +2,11 @@ package task1;
 
 public class StartApp {
     public static void main(String[] args) {
-        String[] data = {"Абакум", "Абакум", "Абакум", "Абакум", "Абакум", "Абакум", "Абрам", "Абросим", "Аввакум",
+        String [] data = {"Абакум", "Абакум", "Абакум", "Абакум", "Абакум", "Абакум", "Абрам", "Абросим", "Аввакум",
                 "Август", "Авдий", "Авель", "Авенир", "Аверий", "Аверкий", "Аверьян", "Авксентий", "Аверкий"
                 , "Аверьян", "Авксентий", "Авксентий" };
         WordAnalyser analyser = new WordAnalyser();
-        System.out.println("Список уникальных слов : "+analyser.analyze(data).keySet());
-        System.out.println("Количество повторений : "+ analyser.analyze(data));
+        System.out.println( "Список уникальных слов : " + analyser.analyze(data).keySet() );
+        System.out.println( "Количество повторений : " + analyser.analyze(data) );
     }
 }
--- src/task1/WordAnalyser.java
+++ src/task1/WordAnalyser.java
@@ -3,12 +3,13 @@ package task1;
 import java.util.HashMap;
 
 public class WordAnalyser {
-    public HashMap<String, Integer> analyze(String[] array) {
-        HashMap<String, Integer> map = new HashMap<>();
+    public HashMap <String, Integer> analyze(String [] array) {
+        HashMap <String, Integer> map = new HashMap<>();
         for (String line : array) {
             if (map.isEmpty() || !map.containsKey(line)) {
                 map.put(line, 1);
-            } else {
+            }
+            else {
                 map.put(line, map.get(line) + 1);
             }
         }
