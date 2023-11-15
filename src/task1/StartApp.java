6c707f742c86e016e395727b5088089d23cf94ef
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-16 00:31:56 +0300

    commit

--- src/task1/StartApp.java
+++ src/task1/StartApp.java
@@ -7,18 +7,33 @@ public class StartApp {
                 , "Аверьян", "Авксентий", "Авксентий" };
         WordAnalyser analyser = new WordAnalyser();
 
+        long start = System.currentTimeMillis();
         System.out.println( "Список уникальных слов : " + analyser.analyze1 (data).keySet() );
         System.out.println( "Количество повторений : " + analyser.analyze1 (data) );
+        long finish = System.currentTimeMillis();
+        long time = finish-start;
+        System.out.println(time);
 
+        start = System.currentTimeMillis();
         System.out.println( "Список уникальных слов : " + analyser.analyze2 (data).keySet() );
         System.out.println( "Количество повторений : " + analyser.analyze2 (data) );
+         finish = System.currentTimeMillis();
+        time = finish-start;
+        System.out.println(time);
 
+        start = System.currentTimeMillis();
         System.out.println( "Список уникальных слов : " + analyser.analyze3 (data).keySet() );
         System.out.println( "Количество повторений : " + analyser.analyze3 (data) );
+        finish = System.currentTimeMillis();
+        time = finish-start;
+        System.out.println(time);
 
+        start = System.currentTimeMillis();
         System.out.println( "Список уникальных слов : " + analyser.analyze4 (data).keySet() );
         System.out.println( "Количество повторений : " + analyser.analyze4 (data) );
-
+        finish = System.currentTimeMillis();
+        time = finish-start;
+        System.out.println(time);
 
     }
 }
