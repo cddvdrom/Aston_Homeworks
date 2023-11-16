37a940eba979ac6274244c5a4b2f548ce99b738c
Denis Boronin <77019249+cddvdrom@users.noreply.github.com>
2023-11-16 17:33:03 +0300

    Update StartTask1.java

--- src/task1/StartTask1.java
+++ src/task1/StartTask1.java
@@ -2,7 +2,7 @@ package task1;
 
 import java.util.List;
 public class StartTask1 {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         GeneratorRandom generator = new GeneratorRandom();
         List <Integer>list = generator.generateList();
         System.out.println ("Случайные числа : " + list);
