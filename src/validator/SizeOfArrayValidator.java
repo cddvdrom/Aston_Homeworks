93fe8296fc250a1b4c0ddab070e731446af43c33
Denis Boronin <77019249+cddvdrom@users.noreply.github.com>
2023-11-17 13:05:01 +0300

    Update SizeOfArrayValidator.java

--- src/validator/SizeOfArrayValidator.java
+++ src/validator/SizeOfArrayValidator.java
@@ -2,7 +2,7 @@ package validator;
 
 import data.ArraySize;
 public class SizeOfArrayValidator {
-    public boolean isValid(int size) {
+    public boolean isValid (int size) {
         return size == ArraySize.RIGHT_SIZE.getSize();
     }
 }
