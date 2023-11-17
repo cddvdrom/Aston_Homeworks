5754c7e280981504865fa062460df13e45fac5c6
Denis Boronin <77019249+cddvdrom@users.noreply.github.com>
2023-11-17 13:05:25 +0300

    Update MyArrayDataException.java

--- src/exception/MyArrayDataException.java
+++ src/exception/MyArrayDataException.java
@@ -1,7 +1,7 @@
 package exception;
 
 public class MyArrayDataException extends Exception{
-    public MyArrayDataException(String message){
+    public MyArrayDataException (String message) {
         super("Неверные данные в ячейке "+message);
     }
 }
