356f91a50da71a811d5ab8016a43b77ee9236a57
Denis Boronin <77019249+cddvdrom@users.noreply.github.com>
2023-11-17 13:03:08 +0300

    Update StartApplication.java

--- src/StartApplication.java
+++ src/StartApplication.java
@@ -3,7 +3,7 @@ import data.MyArrays;
 
 public class StartApplication {
     public static void main(String[] args) {
-        ArrayResearcher researcher=new ArrayResearcher();
+        ArrayResearcher researcher = new ArrayResearcher();
         researcher.outputInfo(MyArrays.RIGHT_ARRAY.getArray());
         researcher.outputInfo(MyArrays.WRONG_SIZE_ARRAY.getArray());
         researcher.outputInfo(MyArrays.WRONG_DATA_ARRAY.getArray());
