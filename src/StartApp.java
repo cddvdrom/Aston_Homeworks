500de70f0f616f16b356faf56084e00914fd2fb4
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-20 11:20:02 +0300

    commit

--- src/StartApp.java
+++ src/StartApp.java
@@ -4,7 +4,7 @@ public class StartApp {
         Box <Orange> box2 = new Box <> ();
         Box <Apple> box3 = new Box <> ();
         box.addFruit (new Apple(),6);
-        box2.addFruit( new Orange(),4);
+        box2.addFruit(new Orange(),4);
         System.out.println(box);
         System.out.println(box2);
         System.out.println(box.compare(box2));
