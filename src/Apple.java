22748532893b5411195933ed5efda2d55e2e4ab5
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-28 01:49:12 +0300

    commit

--- src/Apple.java
+++
@@ -1,5 +0,0 @@
-public class Apple extends Fruit <Apple>{
-    public Apple() {
-        super("яблоко",1.0f);
-    }
-}
--- src/Orange.java
+++
@@ -1,5 +0,0 @@
-public class Orange extends Fruit <Orange>{
-    public Orange() {
-        super("апельсин",1.5f);
-    }
-}
