4de18416a5b06d7c10e13ae97026aeb901bafa0d
Denis Boronin <77019249+cddvdrom@users.noreply.github.com>
2023-11-29 16:52:29 +0300

    Update StartApplication.java

--- src/main/java/ru/boronin/StartApplication.java
+++ src/main/java/ru/boronin/StartApplication.java
@@ -2,7 +2,7 @@ package ru.boronin;
 
 public class StartApplication {
     public static void main(String[] args) {
-        Factorial factorial = new Factorial();
+        Factorial factorial = new Factorial ();
         System.out.println(factorial.calculateRec(50));
         System.out.println(factorial.calculateStream(50));
         System.out.println(factorial.calculateLoop(50));
