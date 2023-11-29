6c61e154f5bd7a14520f66bdfe8710e5b1b80e73
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-11-29 16:39:22 +0300

    commit

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,13 +1,13 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 import ru.boronin.Factorial;
 
 import java.math.BigInteger;
 
 public class FactorialTest {
     public static Factorial factorial;
-    @BeforeAll
+    @BeforeClass
     public static void setup (){
         factorial = new Factorial();
     }
@@ -15,36 +15,36 @@ public class FactorialTest {
     public void factorialLoopTest (){
     BigInteger actual = factorial.calculateLoop(50);
         BigInteger expected = new BigInteger("30414093201713378043612608166064768844377641568960512000000000000");
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
     @Test
     public void factorialStreamTest (){
         BigInteger actual = factorial.calculateStream(50);
         BigInteger expected = new BigInteger("30414093201713378043612608166064768844377641568960512000000000000");
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
     @Test
     public void factorialRecursion (){
         BigInteger actual = factorial.calculateRec(50);
         BigInteger expected = new BigInteger("30414093201713378043612608166064768844377641568960512000000000000");
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
     @Test
     public void factorialLoopTestOfNull (){
         BigInteger actual = factorial.calculateLoop(0);
         BigInteger expected = BigInteger.ONE;
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
     @Test
     public void factorialStreamTestOfNull (){
         BigInteger actual = factorial.calculateStream(0);
         BigInteger expected = BigInteger.ONE;
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
     @Test
     public void factorialRecursionTestOfNull (){
         BigInteger actual = factorial.calculateRec(0);
         BigInteger expected = BigInteger.ONE;
-        Assertions.assertEquals(actual,expected);
+        Assert.assertEquals(actual,expected);
     }
 }
